34c00f5663d0fd12070bfb758a91f4f52b0ee480
katloeffler18 <katloeffler18@gmail.com>
2026-04-19 18:43:14 -0600

    Removed extraneous code from app.js that had been moved to HomePage.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
-
-const apiKey = process.env.REACT_APP_API_BASE_URL;
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch(apiKey)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Backend response:", data);
-        setMessage(data.message);
-      })
-      .catch((err) => {
-        console.error("Error connecting to backend:", err);
-        setMessage("Error connecting to backend");
-      });
-  }, []);
-
-  return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Password Manager</h1>
-      <p>Backend says: {message}</p>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
