e347b468f6868ca1f3bbe2bb9a5bf3d287fb6bdc
sp3ncerlan <lansp329@gmail.com>
2026-04-27 18:16:48 -0500

    feat: login page
    boilerplate login form with bootstrap class CSS

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,15 +1,59 @@
-/**
- * Placeholder of login page
- * @returns LoginPage
- */
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
-function LoginPage() {
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // login function from AuthContext, redirect to vault afterwards
+    login(email, password);
+    navigate("/vault");
+  }
+
+  if (isAuthenticated) {
+    navigate("/vault");
+  }
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Login</h1>
-      <p>Login form will go here.</p>
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="card p-4 shadow" style={{ width: "400px" }}>
+        <h1 className="text-center mb-4">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Email:</label>
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password:</label>
+            <input
+              className="form-control"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
 export default LoginPage;
