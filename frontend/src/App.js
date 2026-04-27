ea6316cf52de3ae8428c797485e8d0aecbae35ed
sp3ncerlan <lansp329@gmail.com>
2026-04-27 18:07:55 -0500

    #21
    feat: started building out the router and navbar foundational layout.
    - installed bootstrap to use template components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import AppNavbar from "./components/AppNavbar";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import { useAuth } from "./context/AuthContext";
 
-function App() {
-  return <HomePage />;
+const App = () => {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isAuthenticated ? <Navigate to="/vault" /> : <LoginPage /> } />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ frontend/src/components/AppNavbar.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const AppNavbar = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    Secure Password Manager
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/vault">
+                            Vault
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default AppNavbar;
