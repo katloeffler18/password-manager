0ea50eaac1266c79234b762d774351711b9298fb
sp3ncerlan <lansp329@gmail.com>
2026-04-28 17:34:21 -0500

    #21 temporary workaround for session states - will need to utilize JWT or session based authentication for PROD

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,60 +3,69 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { login, isAuthenticated } = useAuth();
-  const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const { login, isAuthenticated } = useAuth();
+	const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    
-    // login function from AuthContext, redirect to vault afterwards
-    login(email, password);
-    navigate("/vault");
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
 
-  if (isAuthenticated) {
-    navigate("/vault");
-  }
+		// login function from AuthContext, redirect to vault afterwards
+		login(email, password);
 
-  return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-      <div className="card p-4 shadow" style={{ width: "400px" }}>
-        <h1 className="text-center mb-4">Login</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label className="form-label">Email:</label>
-            <input
-              className="form-control"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <label>Password:</label>
-            <input
-              className="form-control"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary w-100 mb-3">
-            Login
-          </button>
-        </form>
-        <button className="btn btn-secondary w-100" onClick={() => navigate("/")}>
-          Back
-        </button>
-      </div>
-    </div>
-  )
-}
+		if (isAuthenticated) {
+			navigate("/vault");
+		}
+	};
+
+	if (isAuthenticated) {
+		navigate("/vault");
+	}
+
+	return (
+		<div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+			<div className="card p-4 shadow" style={{ width: "400px" }}>
+				<h1 className="text-center mb-4">Login</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="mb-3">
+						<label className="form-label">Email:</label>
+						<input
+							className="form-control"
+							type="email"
+							id="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+						/>
+					</div>
+					<div className="mb-3">
+						<label>Password:</label>
+						<input
+							className="form-control"
+							type="password"
+							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</div>
+					<button
+						type="submit"
+						className="btn btn-primary w-100 mb-3"
+					>
+						Login
+					</button>
+				</form>
+				<button
+					className="btn btn-secondary w-100"
+					onClick={() => navigate("/")}
+				>
+					Back
+				</button>
+			</div>
+		</div>
+	);
+};
 
 export default LoginPage;
